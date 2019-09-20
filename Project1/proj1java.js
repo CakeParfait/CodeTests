3c09a7b688d0406ea7ccb8a25a8188e3e220387e
CakeParfait <55562320+CakeParfait@users.noreply.github.com>
2019-09-19 20:04:41 -0400

    Project 1

---
+++ Project1/proj1java.js
@@ -0,0 +1,120 @@
+// JavaScript Document
+
+//Level4-2
+
+sunOff= new Image (946, 649); 
+sunOff.src = "images/sun_off.png";
+
+sunOn= new Image (946, 649); 
+sunOn.src = "images/sun_move.gif";
+
+
+//Level4-1
+
+buttOff= new Image (380, 283); 
+buttOff.src = "images/butt_off.png";
+
+buttOn= new Image (380, 283); 
+buttOn.src = "images/butt_move.gif";
+
+//Level3-3
+
+scorOff= new Image (605, 471); 
+scorOff.src = "images/Scorp_Off.png";
+
+scorOn= new Image (605, 471); 
+scorOn.src = "images/Scorp_move.gif";
+
+humOff= new Image (334, 228); 
+humOff.src = "images/Human_Off.png";
+
+humOn= new Image (334, 228); 
+humOn.src = "images/Human_On.png";
+
+rustOff= new Image (325, 143); 
+rustOff.src = "images/Rust_Off.png";
+
+rustOn= new Image (325, 143); 
+rustOn.src = "images/Rust_On.png"
+
+eduOff= new Image (506, 109); 
+eduOff.src = "images/Edu_Off.png";
+
+eduOn= new Image (506, 109); 
+eduOn.src = "images/Edu_On.png";
+
+controlOff= new Image (579, 106); 
+controlOff.src = "images/Control_Off.png";
+
+controlOn= new Image (579, 106); 
+controlOn.src = "images/Control_On.png";
+
+lawOff= new Image (453, 113); 
+lawOff.src = "images/Law_Off.png";
+
+lawOn= new Image (453, 113); 
+lawOn.src = "images/Law_On.png";
+
+//Level3-2
+
+waterOff= new Image (1366, 768); 
+waterOff.src = "images/L3-2-Background.png";
+
+waterOn= new Image (1366, 768); 
+waterOn.src = "images/Star_move.gif";
+
+//Level3-1
+
+branchOff= new Image (1366, 768); 
+branchOff.src = "images/branch_off.png";
+
+branchOn= new Image (1366, 768); 
+branchOn.src = "images/branch_move.gif";
+
+grassOff= new Image (1235, 394); 
+grassOff.src = "images/grass_off.png";
+
+grassOn= new Image (1235, 394); 
+grassOn.src = "images/Grass_move.gif";
+
+
+//Level2-2
+
+libraOff= new Image (716, 361); 
+libraOff.src = "images/libra_off.png";
+
+libraOn= new Image (716, 361); 
+libraOn.src = "images/libra_on.png";
+
+scorpOff= new Image (645, 497); 
+scorpOff.src= "images/scorpiooff.png";
+
+scorpOn= new Image (645, 497); 
+scorpOn.src = "images/scorpio_on.png";
+
+//Level2-1
+
+rivOff= new Image (1211, 342); 
+rivOff.src = "images/river_off.png";
+
+rivOn= new Image (1211, 342); 
+rivOn.src = "images/river_move.gif";
+
+treesOff= new Image (1303, 636); 
+treesOff.src = "images/trees_off.png";
+
+treesOn= new Image (1303, 636); 
+treesOn.src = "images/Tree_move.gif";
+// Index Page
+
+galOff= new Image (1366, 768); 
+galOff.src = "images/index.png";
+
+galOn= new Image (1366, 768); 
+galOn.src = "images/indexgif2.gif";
+
+earthOff= new Image (346, 336); 
+earthOff.src = "images/earthoff2.png";
+
+earthOn= new Image (346, 336); 
+earthOn.src = "images/earthon2.png";
